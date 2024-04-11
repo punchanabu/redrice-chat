aa0f054b2405f6375396de355764e16e6dea75e8
Jaitnipat <jaitniphat@gmail.com>
2024-04-12 05:08:56 +0700

    Merge remote-tracking branch 'origin/dev' into game_branch

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,15 +1,15 @@
 import { Server, Socket } from "socket.io";
-import jwt from 'jsonwebtoken';
-import { JwtPayload } from 'jsonwebtoken';
-import { PrismaClient } from '@prisma/client';
+import jwt from "jsonwebtoken";
+import { JwtPayload } from "jsonwebtoken";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 interface DecodedToken extends JwtPayload {
-    id: string;
-    email: string;
-    role: string;
-    exp: number;
+  id: string;
+  email: string;
+  role: string;
+  exp: number;
 }
 
 export class UserController {
@@ -26,30 +26,34 @@ export class UserController {
     this.io.on("connection", (socket: Socket) => {
       console.log("A user connected by socketID:", socket.id);
 
-            const token = socket.handshake.headers['authorization'];
-            var userID: string;
-            
-            if(!token) {
-                console.error('No token provided');
-                socket.emit('auth error', 'Error token not provided');
-                socket.disconnect();
-                return;
-            }
+      const token = socket.handshake.headers["authorization"];
+      var userID: string;
 
-            try {
-                const decodedToken: DecodedToken = jwt.verify(token, this.secretKey) as DecodedToken;
-                userID = decodedToken.id;
-                // Check if user not in database
-          if (prisma.user.findUnique({ where: { ID: Number(userID) } }) == null) {
-            socket.emit("auth_error", { message: "User not found in database" });
-            console.error("User not found");
-            socket.disconnect();
-          }
-                console.log('User connected with userID:', userID);
-            } catch (error) {
-                console.error('Error decoding token:', error);
-            }
+      if (!token) {
+        console.error("No token provided");
+        socket.emit("auth error", "Error token not provided");
+        socket.disconnect();
+        return;
+      }
 
+      try {
+        const decodedToken: DecodedToken = jwt.verify(
+          token,
+          this.secretKey
+        ) as DecodedToken;
+        userID = decodedToken.id;
+
+        // Check if user not in database
+        if (prisma.user.findUnique({ where: { ID: Number(userID) } }) == null) {
+          socket.emit("auth_error", { message: "User not found in database" });
+          console.error("User not found");
+          socket.disconnect();
+        }
+
+        console.log("User connected with userID:", userID);
+      } catch (error) {
+        console.error("Error decoding token:", error);
+      }
 
       socket.on("send message", (msg: { to: string; message: string }) => {
         console.log(
